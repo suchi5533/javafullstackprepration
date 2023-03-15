83440556c3fee452a79ef99d5588f2b0f9daeeff
suchi5533 <suchitw867@gmail.com>
2023-03-15 10:34:31 +0530

    Initial commit

---
+++ src/javafullstackprepration/SecondLargestInArrayUsingCollection.java
@@ -0,0 +1,19 @@
+package javafullstackprepration;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class SecondLargestInArrayUsingCollection{  
+public static int getSecondLargest(Integer[] a, int total){  
+List<Integer> list=Arrays.asList(a);  
+Collections.sort(list);  
+int element=list.get(total-2);  
+return element;  
+}  
+public static void main(String args[]){  
+Integer a[]={1,2,5,6,3,2};  
+Integer b[]={44,66,99,77,33,22,55};  
+System.out.println("Second Largest: "+getSecondLargest(a,6));  
+System.out.println("Second Largest: "+getSecondLargest(b,7));  
+}}  
